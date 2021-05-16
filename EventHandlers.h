4b8bec8de2913f34db00f2690e9dcd6886e32333
Montero <corra72@gmail.com>
2021-05-16 18:10:39 +0200

    Update EventHandlers.h

--- EventHandlers.h
+++ EventHandlers.h
@@ -1,19 +1,19 @@
 #pragma once
-#include "structs.h"
+#include "Structs.h"
 #include "Enumerators.h"
 #include "Networking.h"
 #include "CPlayer.h"
 
 /*
-* nota tecnica: le classi puramente virtuali in altri linguaggi sono chiamate "interfacce"; in 
+* nota tecnica: le classi puramente virtuali in altri linguaggi sono chiamate "interfacce"; in
 * c++ non hai le interfacce, ne' hai la necessità di avverle. Tuttavia, per maggiore chiarezza,
 * inizierò a chiamare le classi puramente virtuali (senza metodi definiti e senza proprieta) interfacce
 * ed i loro nomi inizieranno per I
-* 
+*
 */
 
 /*
-* Nota tecnica: per gestire gli eventi ci sono vari modelli: io qui userò un modello che è 
+* Nota tecnica: per gestire gli eventi ci sono vari modelli: io qui userò un modello che è
 * quello di Java semplificato; perché Java? Perché tornerà molto utile nella definizione
 * delle procedure speciali!
 */
@@ -22,9 +22,9 @@
 /*
 * I metodi che iniziano per onXXX sono metodi che vengono chiamati quando un certo evento accade.
 * I campi vengono definiti in base all'oggetto che viene chiamato, ma ogni metodo ha obbligatoriamente
-* due campi: 
+* due campi:
 *  - caller, è l'oggetto che ha inviato l'evento
-*  - handled un booleano che diventa vero se una chiamata della catena ha gestito definitivamente l'evento; 
+*  - handled un booleano che diventa vero se una chiamata della catena ha gestito definitivamente l'evento;
 *    questo interrompe la catena (non credo che lo userai, ma è sempre meglio avercelo).
 */
 class IDestroyableHandler {
